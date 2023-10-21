117827f0565fff021251524957a21bf24b93336d
everinho <agorecky@gmail.com>
2023-10-22 00:00:22 +0200

    calculating asymmetry done

--- app/src/main/java/net/simplifiedcoding/FaceThePalsy/facedetector/FaceBox.kt
+++ app/src/main/java/net/simplifiedcoding/FaceThePalsy/facedetector/FaceBox.kt
@@ -11,6 +11,8 @@ import android.util.Log
 import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceContour
 import com.google.mlkit.vision.face.FaceLandmark
+import net.simplifiedcoding.FaceThePalsy.exercises.FollowBox
+import kotlin.math.abs
 import kotlin.math.sqrt
 import kotlin.math.pow
 
@@ -41,7 +43,7 @@ class FaceBox(
     private val paint_punkt = Paint().apply {
         color = Color.RED
         style = Paint.Style.STROKE
-        strokeWidth = 20.0f
+        strokeWidth = 14.0f
     }
 
     override fun draw(canvas: Canvas?) {
@@ -50,25 +52,63 @@ class FaceBox(
         val left_eye = face.getContour(FaceContour.LEFT_EYE)?.points
         val right_eye = face.getContour(FaceContour.RIGHT_EYE)?.points
 
-        val index1 = 0
-        val index2 = 8
+        val point1 = left_eye?.getOrNull(0)
+        val point2 = left_eye?.getOrNull(8)
 
-        val point1 = left_eye?.getOrNull(index1)
-        val point2 = left_eye?.getOrNull(index2)
-
-        val point3 = right_eye?.getOrNull(index1)
-        val point4 = right_eye?.getOrNull(index2)
+        val point3 = right_eye?.getOrNull(0)
+        val point4 = right_eye?.getOrNull(8)
 
         xyz(canvas, point1, point2)
         xyz(canvas, point3, point4)
+
+        val point25 = left_eye?.getOrNull(3)
+        val point26 = left_eye?.getOrNull(5)
+
+        val point27 = left_eye?.getOrNull(13)
+        val point28 = left_eye?.getOrNull(11)
+
+        val point29 = right_eye?.getOrNull(3)
+        val point30 = right_eye?.getOrNull(5)
+
+        val point31 = right_eye?.getOrNull(13)
+        val point32 = right_eye?.getOrNull(11)
+
+        xyz(canvas, point25, point26)
+        xyz(canvas, point27, point28)
+        xyz(canvas, point29, point30)
+        xyz(canvas, point31, point32)
+
 
         //nose
         val nose = face.getContour(FaceContour.NOSE_BOTTOM)?.points
 
         val point5 = nose?.getOrNull(0)
         val point6 = nose?.getOrNull(2)
+        val point21 = nose?.getOrNull(1)
 
         xyz(canvas, point5, point6)
+
+        //eyebrows
+        val left_eyebrow = face.getContour(FaceContour.LEFT_EYEBROW_TOP)?.points
+        val right_eyebrow = face.getContour(FaceContour.RIGHT_EYEBROW_TOP)?.points
+        val point11 = left_eyebrow?.getOrNull(0)
+        val point12 = left_eyebrow?.getOrNull(1)
+        val point13 = left_eyebrow?.getOrNull(2)
+        val point14 = left_eyebrow?.getOrNull(3)
+        val point15 = left_eyebrow?.getOrNull(4)
+
+        val point16 = right_eyebrow?.getOrNull(4)
+        val point17 = right_eyebrow?.getOrNull(3)
+        val point18 = right_eyebrow?.getOrNull(2)
+        val point19 = right_eyebrow?.getOrNull(1)
+        val point20 = right_eyebrow?.getOrNull(0)
+
+        xyz(canvas, point11, point12)
+        xyz(canvas, point13, point14)
+        xyz(canvas, point15, point16)
+        xyz(canvas, point17, point18)
+        xyz(canvas, point19, point20)
+
 
         //face
         val facial = face.getContour(FaceContour.FACE)?.points
@@ -77,16 +117,90 @@ class FaceBox(
         val point9 = facial?.getOrNull(7)
 
         //mouth
-        val mouth = face.getContour(FaceContour.LOWER_LIP_BOTTOM)?.points
-        val point10 = mouth?.getOrNull(4)
+        val mouth_lower = face.getContour(FaceContour.LOWER_LIP_BOTTOM)?.points
+        val mouth_upper = face.getContour(FaceContour.UPPER_LIP_TOP)?.points
+        val point10 = mouth_lower?.getOrNull(4)
+        val point22 = mouth_upper?.getOrNull(5)
+        val point23 = mouth_upper?.getOrNull(0)
+        val point24 = mouth_upper?.getOrNull(10)
+        val point33 = mouth_upper?.getOrNull(3)
+        val point34 = mouth_upper?.getOrNull(4)
+        val point35 = mouth_lower?.getOrNull(6)
+        val point36 = mouth_lower?.getOrNull(5)
+        val point37 = mouth_upper?.getOrNull(6)
+        val point38 = mouth_upper?.getOrNull(7)
+        val point39 = mouth_lower?.getOrNull(3)
+        val point40 = mouth_lower?.getOrNull(2)
+
 
         xyz(canvas, point7, point8)
         xyz(canvas, point9, point10)
+        xyz(canvas, point21, point22)
+        xyz(canvas, point23, point24)
+        xyz(canvas, point33, point34)
+        xyz(canvas, point35, point36)
+        xyz(canvas, point37, point38)
+        xyz(canvas, point39, point40)
+
+        //distances
+
+        val distance_A = distance(point8,point9)
+        val distance_Bl = distance(point1,point2)
+        val distance_Br = distance(point3,point4)
+        val distance_C = distance(point10,point7)
+        val distance_D = distance(point8,point1)
+        val distance_E = distance(point4,point9)
+        val distance_F = distance(point1,point10)
+        val distance_G = distance(point4,point10)
+        val distance_H = distance(point1,point5)
+        val distance_I = distance(point4,point6)
+        val distance_J = distance(point5,point10)
+        val distance_K = distance(point6,point10)
+        val distance_R = distance(point14,point10)
+        val distance_S = distance(point17,point10)
+        val distance_X = distance(point21,point22)
+        val distance_T = distance(point13,point10)
+        val distance_U = distance(point18,point10)
+        val distance_Vl = distance(point23,point10)
+        val distance_Vr = distance(point24,point10)
+        val distance_W = distance(point23,point24)
+        val distance_Nl = distance(point25,point27)
+        val distance_Nr = distance(point26,point28)
+        val distance_Ol = distance(point29,point31)
+        val distance_Or = distance(point30,point32)
+        val distance_Pi = distance(point33,point35)
+        val distance_Pu = distance(point34,point36)
+        val distance_Qi = distance(point37,point38)
+        val distance_Qu = distance(point39,point40)
+
+        // Dystanse po lewej stronie
+        val distancesLeft = listOf(
+            distance_Bl, distance_D, distance_H, distance_F, distance_J, distance_T, distance_R, distance_Vl, distance_Pi, distance_Pu, distance_Nl, distance_Nr
+        )
+
+        // Dystanse po prawej stronie
+        val distancesRight = listOf(
+            distance_Br, distance_E, distance_I, distance_G, distance_K, distance_U, distance_S, distance_Vr, distance_Qi, distance_Qu, distance_Or, distance_Ol
+        )
+
+        // Oblicz różnice między odpowiadającymi sobie dystansami
+        val differences = distancesLeft.mapIndexed { index, distanceLeft ->
+            abs(distanceLeft - distancesRight[index])
+        }
+
+        // Oblicz sumę różnic
+        val sumOfDifferences = differences.sum()
+
+        // Oblicz średnią różnicę
+        val asymmetry = sumOfDifferences / distancesLeft.size
+
+        // Wynik asymetrii twarzy
+        Log.d("Asymmetry", "Asymmetry: $asymmetry")
+        val asymmetrytext = "Asymetria: $asymmetry"
+        canvas?.drawText(asymmetrytext, 50F, 450F, paint_text)
+
     }
 
-    private fun calculateDistance_simple(x1: Float, y1: Float, x2: Float, y2: Float): Float {
-        return sqrt((x2 - x1).pow(2) + (y2 - y1).pow(2))
-    }
     private fun xyz(canvas: Canvas?,point1: PointF?, point2: PointF?){
         val adjustedPoint1 = PointF(point1!!.x, point1.y)
         val rect1 = getBoxRect(
@@ -115,6 +229,41 @@ class FaceBox(
         )
         val mappedPoint2 = PointF(rect2.centerX(), rect2.centerY())
         canvas?.drawPoint(mappedPoint2.x, mappedPoint2.y, paint_punkt)
+    }
+
+    private fun calculateDistance_simple(x1: Float, y1: Float, x2: Float, y2: Float): Float {
+        return sqrt((x2 - x1).pow(2) + (y2 - y1).pow(2))
+    }
+
+    private fun distance(point1: PointF?, point2: PointF?): Float{
+        val adjustedPoint1 = PointF(point1!!.x, point1.y)
+        val rect1 = getBoxRect(
+            imageRectWidth = imageRect.width().toFloat(),
+            imageRectHeight = imageRect.height().toFloat(),
+            faceBoundingBox = Rect(
+                adjustedPoint1.x.toInt(),
+                adjustedPoint1.y.toInt(),
+                adjustedPoint1.x.toInt(),
+                adjustedPoint1.y.toInt()
+            )
+        )
+        val mappedPoint1 = PointF(rect1.centerX(), rect1.centerY())
+
+        val adjustedPoint2 = PointF(point2!!.x, point2.y)
+        val rect2 = getBoxRect(
+            imageRectWidth = imageRect.width().toFloat(),
+            imageRectHeight = imageRect.height().toFloat(),
+            faceBoundingBox = Rect(
+                adjustedPoint2.x.toInt(),
+                adjustedPoint2.y.toInt(),
+                adjustedPoint2.x.toInt(),
+                adjustedPoint2.y.toInt()
+            )
+        )
+        val mappedPoint2 = PointF(rect2.centerX(), rect2.centerY())
+
+        val distance = calculateDistance_simple(mappedPoint1.x,mappedPoint1.y, mappedPoint2.x,mappedPoint2.y)
+        return distance
     }
 
 }
